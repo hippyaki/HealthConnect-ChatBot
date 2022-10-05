e81ccb28f6b9686e5d7c772ad85822f156cc62a6
Akshayan Sinha <akshayan.sinha@gmail.com>
2022-10-05 10:28:13 +0530

    welcome page

--- src/components/welcome/template.js
+++ src/components/welcome/template.js
@@ -8,23 +8,16 @@ const template = (context) => {
       <h5 class="card-title">Terms & Conditions</h5>
       <div class="card-text">
         <p>
-          
-          1. The information on this bot has been included in good faith for general informational purposes only. The bot may contain information submitted by a third party including adverts.
-
+          1. The information on this bot has been included in good faith for general informational purposes only.
         </p>
         <p>
-          
-          2. The information should not be relied upon for any specific purpose and no representation or warranty is given as to its accuracy or completeness.
-
+          2. The information should not be relied on, and it is only an near-value estimate of the possible symptom.
         </p>
         <p>
-          
           3. Any opinions (express or implied) are those of the AI Bot based on the structured code by Infermedica.
         </p>
         <p>
-          
           4. Do not use make copies of the AI Bot. Developer source code available from official site of Infermedica.
-
         </p>
         <p class="text-muted small">
           <i class="fa fa-info-circle"></i> 
